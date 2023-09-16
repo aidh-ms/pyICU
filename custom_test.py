00397069b99875ff8029b8da37afb927b5d4ae38
aegis301 <57251443+aegis301@users.noreply.github.com>
2023-09-16 14:13:15 +0200

    fix: add text conversion for query

--- custom_test.py
+++ custom_test.py
@@ -1,17 +1,19 @@
-from pyICU.connection.key import mimic_engine
-from pyICU.connection.Connector import SQLDBConnector
+from pyICU.connection.key import mimic_demo_engine
+from pyICU.connection.Connector import SQLDBConnector, MimicConnector
 
 
 def test_connection():
     """Test connection to database and concept loading."""
-    connector = SQLDBConnector(mimic_engine)
+    connector = SQLDBConnector(mimic_demo_engine)
     print(connector.concepts)
 
 
 def test_mimic_connector():
     """Test connection to mimic database and concept loading."""
-    connector = SQLDBConnector(mimic_engine)
+    connector = MimicConnector(mimic_demo_engine)
     print(connector.concepts)
+    print(connector.execute_query(
+        "SELECT * FROM mimiciv_derived.icustay_detail LIMIT 10;"))
 
 
 if __name__ == '__main__':
