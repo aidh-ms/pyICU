3f26023e760f9418f282979a9065df19be4d0874
aegis301 <57251443+aegis301@users.noreply.github.com>
2023-10-09 14:08:35 +0200

    chore: update dependencies in setup

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
 from setuptools import setup
 
-setup(pbr=True)
+setup(
+    pbr=True,
+    name='pyICU',
+    version='0.0.1',
+    description='Python package containing different functionalities for working with ICU data.',
+    author='Christian Porschen',
+    author_email='christian.porschen@ukmuenster.de',
+    packages=['pyICU'],
+    install_requires=[
+        'colorlog>=6.7.0',
+        'numpy>=1.25.2',
+        'psycopg2>=2.9.7',
+        'pandas>=2.0.3',
+        'SQLAlchemy>=1.4.49',
+    ],
+)
